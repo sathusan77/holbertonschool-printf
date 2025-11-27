f59c3497e8286ac671b09f835b99169f6eec8857
jeyakumar Sathusan <jeyakumar.sathusan@gmail.com>
2025-11-27 14:01:18 +0000

    o

--- _printf.c
+++ _printf.c
@@ -1,53 +1,29 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
 
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-    int i = 0, count = 0;
-    va_list args;
+	va_list args;
+	int count = 0;
 
-    if (!format)
-        return (-1);
-
-    va_start(args, format);
-
-    while (format[i])
-    {
-        if (format[i] == '%' && format[i + 1])
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-            }
-            else if (format[i] == 's')
-            {
-                char *str = va_arg(args, char *);
-                int j = 0;
-
-                if (!str)
-                    str = "(null)";
-                while (str[j])
-                    count += write(1, &str[j++], 1);
-            }
-            else if (format[i] == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+				count += _putchar(va_arg(args, int));
+		}
+		else
+			count += _putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
 
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Hello %s ! I print a char: %c and a percent: %%\n", "Sathu", 'A');
+    return (0);
+}
+
