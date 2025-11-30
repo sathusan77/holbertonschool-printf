5a1bc2af17ae1b3b13fe3b0c3dd11f357c584a2a
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-11-30 01:10:28 +0000

    o

--- _printf.c
+++ _printf.c
@@ -1,42 +1,6 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: character to print
- * Return: 1 on success
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_number - prints an integer to stdout
- * @n: integer to print
- * Return: number of characters printed
- */
-int print_number(int n)
-{
-	int count = 0;
-	unsigned int num;
-
-	if (n < 0)
-	{
-		count += _putchar('-');
-		num = -n;
-	}
-	else
-		num = n;
-
-	if (num / 10)
-		count += print_number(num / 10);
-
-	count += _putchar((num % 10) + '0');
-
-	return (count);
-}
-
-/**
  * handle_percent - handles a conversion specifier
  * @specifier: the character after %
  * @args: va_list of arguments
@@ -44,34 +8,34 @@ int print_number(int n)
  */
 int handle_percent(char specifier, va_list args)
 {
-	int count = 0;
-	char *str;
-	char c;
+    int count = 0;
+    char *str;
+    char c;
 
-	if (specifier == 'c')
-	{
-		c = va_arg(args, int);
-		count += _putchar(c);
-	}
-	else if (specifier == 's')
-	{
-		str = va_arg(args, char *);
-		if (!str)
-			str = "(null)";
-		while (*str)
-			count += _putchar(*str++);
-	}
-	else if (specifier == '%')
-		count += _putchar('%');
-	else if (specifier == 'd' || specifier == 'i')
-		count += print_number(va_arg(args, int));
-	else
-	{
-		count += _putchar('%');
-		count += _putchar(specifier);
-	}
+    if (specifier == 'c')
+    {
+        c = va_arg(args, int);
+        count += _putchar(c);
+    }
+    else if (specifier == 's')
+    {
+        str = va_arg(args, char *);
+        if (!str)
+            str = "(null)";
+        while (*str)
+            count += _putchar(*str++);
+    }
+    else if (specifier == '%')
+        count += _putchar('%');
+    else if (specifier == 'd' || specifier == 'i')
+        count += print_number(va_arg(args, int));
+    else
+    {
+        count += _putchar('%');
+        count += _putchar(specifier);
+    }
 
-	return (count);
+    return (count);
 }
 
 /**
@@ -81,32 +45,32 @@ int handle_percent(char specifier, va_list args)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0;
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			count += handle_percent(*format, args);
-		}
-		else
-			count += _putchar(*format);
-		format++;
-	}
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')
+            {
+                va_end(args);
+                return (-1);
+            }
+            count += handle_percent(*format, args);
+        }
+        else
+            count += _putchar(*format);
+        format++;
+    }
 
-	va_end(args);
-	return (count);
+    va_end(args);
+    return (count);
 }
 
---
+++ print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int print_number(int n)
+{
+    int count = 0;
+    unsigned int num;
+
+    if (n < 0)
+    {
+        count += _putchar('-');
+        num = -n;
+    }
+    else
+        num = n;
+
+    if (num / 10)
+        count += print_number(num / 10);
+
+    count += _putchar((num % 10) + '0');
+
+    return (count);
+}
+
