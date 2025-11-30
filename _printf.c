6e99094bbc4a2797bd38e9b1cfa648d49312d3f0
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2025-11-30 02:06:50 +0000

    o

--- _printf.c
+++ _printf.c
@@ -8,70 +8,37 @@
  */
 int handle_percent(char specifier, va_list args)
 {
-    int count = 0;
-    char *str;
-    char c;
+	int count = 0;
+	char *str;
+	char c;
 
-    if (specifier == 'c')
-    {
-        c = va_arg(args, int);
-        count += _putchar(c);
-    }
-    else if (specifier == 's')
-    {
-        str = va_arg(args, char *);
-        if (!str)
-            str = "(null)";
-        while (*str)
-            count += _putchar(*str++);
-    }
-    else if (specifier == '%')
-        count += _putchar('%');
-    else if (specifier == 'd' || specifier == 'i')
-        count += print_number(va_arg(args, int));
-    else
-    {
-        count += _putchar('%');
-        count += _putchar(specifier);
-    }
+	if (specifier == 'c')
+	{
+		c = va_arg(args, int);
+		count += _putchar(c);
+	}
+	else if (specifier == 's')
+	{
+		str = va_arg(args, char *);
+		if (!str)
+			str = "(null)";
+		while (*str)
+			count += _putchar(*str++);
+	}
+	else if (specifier == '%')
+		count += _putchar('%');
+	else if (specifier == 'd' || specifier == 'i')
+		count += print_number(va_arg(args, int));
+	else
+	{
+		count += _putchar('%');
+		count += _putchar(specifier);
+	}
 
-    return (count);
+	return (count);
 }
 
 /**
  * _printf - produces output according to a format
- * @format: format string
- * Return: number of characters printed, or -1 on error
- */
-int _printf(const char *format, ...)
-{
-    va_list args;
-    int count = 0;
-
-    if (!format)
-        return (-1);
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (!*format)  /* % à la fin de la chaîne */
-            {
-                va_end(args);
-                return (-1);
-            }
-            count += handle_percent(*format, args);
-        }
-        else
-            count += _putchar(*format);
-
-        format++;
-    }
-
-    va_end(args);
-    return (count);
-}
+ * @format: format strin*
 
